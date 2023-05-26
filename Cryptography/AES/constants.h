9a90a952bb449981ac5063b1d732ffd0cbc6c464
martian <26498920+martian0x80@users.noreply.github.com>
2023-05-26 23:30:40 +0530

    In reference to issue #161

---
+++ Cryptography/AES/constants.h
@@ -0,0 +1,20 @@
+//
+// Created by martian on 3/29/2023.
+//
+
+#ifndef AES_CONSTANTS_H
+#define AES_CONSTANTS_H
+
+#include <stdint.h>
+
+#define AES_128_N_k 4
+#define AES_128_N_r 10
+
+#define AES_192_N_k 6
+#define AES_192_N_r 12
+
+#define AES_256_N_k 8
+#define AES_256_N_r 14
+
+
+#endif //AES_CONSTANTS_H
